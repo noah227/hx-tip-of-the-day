54b42936b68e298c595334e2f6c72f4ca3ee95c3
noah227 <noah227@foxmail.com>
2024-04-08 10:07:46 +0800

    feat: 添加一天只提示一次的设定（未启用）

--- extension.js
+++ extension.js
@@ -1,8 +1,25 @@
 var hx = require("hbuilderx");
 const main = require("./main.js")
+
+const shallActivateInstantly = () => {
+	// 配置
+	const activateOnLaunch = hx.workspace.getConfiguration(require("./package.json").id).get("activateOnLaunch")
+	return activateOnLaunch
+	// 一天只显示一次
+	let alreadyShown = false
+	const fs = require("fs")
+	const path = require("path")
+	const _ = path.resolve(__dirname, ".record")
+	if(fs.existsSync(_)) {
+		const s = fs.readFileSync(_, {encoding: "utf8"}).trim()
+		alreadyShown = (Date.now() - new Date(s).getTime()) < 864e5
+	}
+	return activateOnLaunch && !alreadyShown
+}
+
 //该方法将在插件激活的时候调用
 function activate(context) {
-	if(hx.workspace.getConfiguration(require("./package.json").id).get("activateOnLaunch")) { 
+	if(shallActivateInstantly()) { 
 		main.renderTip(context, true)
 	}
 	let disposable = hx.commands.registerCommand('extension.showTip', (ctx) => {
