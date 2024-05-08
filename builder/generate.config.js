ec41ca512b25093b231a1a8c4a11460acde6b79b
noah227 <noah227@foxmail.com>
2024-05-08 10:33:10 +0800

    chore: 添加更多的文档内容

--- builder/generate.config.js
+++ builder/generate.config.js
@@ -1,8 +1,20 @@
 module.exports = {
 	docs: {
 		allowedList: [
-			"skill.md", "goto.md", "find.md",
-			"node-multi-file-search.md"
+			// 高校极客技巧
+			"skill.md", 
+			// 跳转/转到定义
+			"goto.md", 
+			// 查找/替换
+			"find.md",
+			// 多文件字符搜索
+			"node-multi-file-search.md",
+			// 折叠
+			"fold.md",
+			// 格式化
+			"format.md",
+			// 多窗体
+			"multi-window.md"
 		]
 	}
 }
