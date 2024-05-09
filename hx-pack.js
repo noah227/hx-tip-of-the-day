f4058d5326f0fff4a5ccd38228cbf51705cda163
noah227 <noah227@foxmail.com>
2024-05-09 10:45:02 +0800

    chore: 添加打包配置

---
+++ hx-pack.js
@@ -0,0 +1,21 @@
+const cozip = require("cozip")
+const publishName = `${require("./package.json").name}.zip`
+
+;(() => {
+    cozip(publishName, [
+        ["./extension.js", false],
+        ["./config.helper.js", false], 
+        ["./main.js", false],
+        ["./main.utils.js", false],
+        ["./styles.css", false],
+        ["./package.json", false],
+        ["./node_modules/hx-configuration-helper", true],
+        ["./node_modules/marked", true],
+        ["./tips", true],
+    ], err => {
+        if (err) console.error(err)
+        else {
+            console.log("打包完成, 文件大小", (require("fs").statSync(publishName).size / 1024).toFixed(2), "KB")
+        }
+    })
+})()
